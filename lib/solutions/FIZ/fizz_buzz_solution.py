8d0809e017f4c55d31ec6e269a0883f1f672c621
sbx_user1060 <sbx_user1060@ip-10-128-253-22.eu-west-2.compute.internal>
2018-11-25 14:26:43 +0000

    Sun Nov 25 14:26:43 UTC 2018

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -9,15 +9,25 @@ def fizz_buzz(number):
     #find 5 in string
     five = re.findall('5', str_num)
 
-    if ((number % 3 == 0) and (number % 5 == 0)) or (three == '3' and five == '5'):
+    #find 3 or five in number
+    if(three == '3' and five == '5'):
+        bool_fizz_buzz = 3
+    elif (three == '3'):
+        bool_fizz_buzz = 1
+    elif (five == '5'):
+        bool_fizz_buzz = 2
+    else:
+        bool_fizz_buzz = 0
+
+    if ((number % 3 == 0) and (number % 5 == 0)) or (bool_fizz_buzz == 3):
         return "fizz buzz"
-    elif (number % 3 == 0) or (three == '3'):
+    elif (number % 3 == 0) or (bool_fizz_buzz == 1):
         return "fizz"
-    elif (number % 5 == 0) or (five == '5'):
+    elif (number % 5 == 0) or (bool_fizz_buzz == 2):
         return "buzz"
     else:
         return number
 
 
 
-print fizz_buzz('12')
+print fizz_buzz(12345)
